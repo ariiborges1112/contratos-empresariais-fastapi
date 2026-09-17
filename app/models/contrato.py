8677c1622dd3d6abd04595db03e6b4849bd9573f
ariiborges1112 <ari.borges1112@gmail.com>
2026-09-17 16:06:04 -0300

    refactor: remove import Field

--- app/models/contrato.py
+++ app/models/contrato.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, model_validator
 from datetime import datetime, date
 from typing import Optional
 
