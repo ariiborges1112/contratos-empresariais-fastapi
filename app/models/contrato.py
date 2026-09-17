d49f478904ba9f0cb244b0de24fa070c7096be03
ariiborges1112 <ari.borges1112@gmail.com>
2026-09-17 14:49:42 -0300

    feat: cria classe enum para situacao dos contratos

--- app/models/contrato.py
+++ app/models/contrato.py
@@ -1,2 +1,19 @@
-from pydantic import BaseModel
+from enum import Enum
+from pydantic import BaseModel, Field
+from datetime import datetime, date
+from typing import Optional
 
+class SituacaoContrato(str, Enum):
+    VIGENTE = "vigente"
+    VENCIDO = "vencido"
+    PROXIMO_VENCIMENTO = "proximo_vencimento" #serve pra representar os contratos prestes a vencer
+
+class ContratoBase(SituacaoContrato):
+    def aa(self, nome_original: str, descricao: str, categoria: str,
+           contratante: str, contratado: str, data_inicio: date, data_termino: date):
+
+
+class ContratoArmazenado(ContratoBase):
+    def aa(self, id: int, nome_armazenado: str, tipo_mime: str, 
+           categoria: str, sha256: str, tamanho: int, data_upload: datetime):
+    pass
