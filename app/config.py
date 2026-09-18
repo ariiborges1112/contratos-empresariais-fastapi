771e07af14dacf422279f5f88747b2f423e657a1
joaopedro-creator <joaopedro0744@gmail.com>
2026-09-18 20:52:12 -0300

    criando submodelos

--- app/config.py
+++ app/config.py
@@ -11,3 +11,8 @@ from pydantic_settings import (
 BASE_DIR = Path(__file__).resolve().parent
 YAML_PATH = BASE_DIR / "config.yaml"
 
+class StorageSentings(BaseModel):
+    diretorio_documentos:str = "./storage/documentos"
+    diretorio_metadata: str = "./storage/metadata"
+    diretorio_backups: str = "./storage/backups"
+    diretorio_exportados: str = "./storage/exportados"
