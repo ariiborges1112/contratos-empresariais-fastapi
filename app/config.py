b2b9d6b4fa232178d67abef21107524086c65428
joaopedro-creator <joaopedro0744@gmail.com>
2026-09-19 13:36:36 -0300

    criando classes(BaseModel)

--- app/config.py
+++ app/config.py
@@ -11,8 +11,23 @@ from pydantic_settings import (
 BASE_DIR = Path(__file__).resolve().parent
 YAML_PATH = BASE_DIR / "config.yaml"
 
-class StorageSentings(BaseModel):
+class StorageSenttings(BaseModel):
     diretorio_documentos:str = "./storage/documentos"
     diretorio_metadata: str = "./storage/metadata"
     diretorio_backups: str = "./storage/backups"
     diretorio_exportados: str = "./storage/exportados"
+
+
+class UploadSenttings(BaseModel):
+    tamanho_max: int = 20
+
+
+class HashSenttings(BaseModel):
+    algoritmo: str = "sha256"
+
+class LoggingSentings(BaseModel):
+    arquivo: str = "./storage/logs/sistema.log"
+    nivel: str = "INFO"
+
+class BackupSenttings(BaseModel):
+    formato: str = "zip"
