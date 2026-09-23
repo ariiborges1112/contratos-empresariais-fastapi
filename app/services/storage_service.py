23d9261f6324edc21a77a198a8086b432fba4a20
ariiborges1112 <ari.borges1112@gmail.com>
2026-09-23 13:53:33 -0300

    faet: ajusta funcao '_ler_catalogo'

--- app/services/storage_service.py
+++ app/services/storage_service.py
@@ -9,20 +9,30 @@ def _ler_catalogo() -> list[ContratoArmazenado]:
     if not os.path.exists(CAMINHO_JSON):
         return []
     else:
-        with open(file=CAMINHO_JSON, mode="r", encoding="utf-8") as documentos:
-            dados_dicionario = json.load(documentos)
+        with open(file=CAMINHO_JSON, mode="r", encoding="utf-8") as arquivo:
+            dados_dicionario = json.load(arquivo)
 
             contratos_validados = []
 
             for i in dados_dicionario:
-                objeto = ContratoArmazenado.model_validate(i)
-                contratos_validados.append(objeto)
+                objeto_validado = ContratoArmazenado.model_validate(i)
+                contratos_validados.append(objeto_validado)
 
             return contratos_validados
 
 def _salvar_catalogo(documentos: list[ContratoArmazenado]):
+    if not os.path.exists(CAMINHO_JSON):
+        return []
+    else:
+        with open(file=CAMINHO_JSON, mode="w", enconding="utf-8") as documentos:
+            dados_objetos = json.dump(ContratoArmazenado, arquivo, indent=4)
+
+            for i in dados_objetos:
+                objeto = ContratoArmazenado.model_dump()
+                
+
+
     
-    ...
 
 def listar_catalogo():
     ...
