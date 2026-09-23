05d2591813a9b14c4342b252051ab4e789716356
ariiborges1112 <ari.borges1112@gmail.com>
2026-09-22 23:41:28 -0300

    feat: implementa caminho dinamico do arquivo

--- app/logger.py
+++ app/logger.py
@@ -1,9 +1,10 @@
+from app.config import settings
 import logging
 import os
 
 log_caminho = "storage/logs"
 os.makedirs(log_caminho, exist_ok=True)
-log_file = os.path.join(log_caminho, "sistema.log")
+log_file = settings.logging.arquivo
 
 log_formato = "%(asctime)s %(levelname)s %(message)s"
 
