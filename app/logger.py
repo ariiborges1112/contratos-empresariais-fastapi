94c6bb6ea664a51938b50388896cc1cee63100ae
ariiborges1112 <ari.borges1112@gmail.com>
2026-09-23 13:45:10 -0300

    refactor: cria a pasta apenas quando a funcao setup_logger() for chaamada

--- app/logger.py
+++ app/logger.py
@@ -3,12 +3,15 @@ import logging
 import os
 
 log_file = settings.logging.arquivo
-log_caminho = os.path.dirname(log_file)
-os.makedirs(log_caminho, exist_ok=True)
 
 log_formato = "%(asctime)s %(levelname)s %(message)s"
 
 def setup_logger():
+    log_caminho = os.path.dirname(log_file)
+
+    if log_caminho:
+        os.makedirs(log_caminho, exist_ok=True)
+        
     formatador = logging.Formatter(
         fmt = log_formato,
         datefmt = "%Y-%m-%d %H:%M:%S" #remove milissegundos
