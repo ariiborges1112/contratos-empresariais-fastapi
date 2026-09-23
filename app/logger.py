f876fe2bc1d2869758e36a59468d70487fe0f504
ariiborges1112 <ari.borges1112@gmail.com>
2026-09-22 23:47:55 -0300

    feat: seguranca para nao criar logs duplicados

--- app/logger.py
+++ app/logger.py
@@ -2,9 +2,9 @@ from app.config import settings
 import logging
 import os
 
-log_caminho = "storage/logs"
-os.makedirs(log_caminho, exist_ok=True)
 log_file = settings.logging.arquivo
+log_caminho = os.path.dirname(log_file)
+os.makedirs(log_caminho, exist_ok=True)
 
 log_formato = "%(asctime)s %(levelname)s %(message)s"
 
@@ -17,9 +17,10 @@ def setup_logger():
     manipulador_arquivo = logging.FileHandler(log_file, encoding="utf-8")
     logger = logging.getLogger()
 
-    logger.setLevel(logging.INFO)
+    logger.setLevel(settings.logging.nivel)
 
-    manipulador_arquivo.setFormatter(formatador)
-    logger.addHandler(manipulador_arquivo)
+    if not logger.handlers:
+        manipulador_arquivo.setFormatter(formatador)
+        logger.addHandler(manipulador_arquivo)
 
     return logger
