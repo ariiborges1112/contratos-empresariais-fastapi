08cbbbc121e85d1357b4f9f60773a219443cdee6
ariiborges1112 <ari.borges1112@gmail.com>
2026-09-23 16:26:44 -0300

    feat: implementa filtros na funcao de listagem

--- app/services/storage_service.py
+++ app/services/storage_service.py
@@ -20,6 +20,7 @@ def _ler_catalogo() -> list[ContratoArmazenado]:
 
             return contratos_validados
 
+#F1
 def _salvar_catalogo(documentos: list[ContratoArmazenado]):
     with open(file=CAMINHO_JSON, mode="w", encoding="utf-8") as arquivo:
         dados_para_salvar = []
@@ -29,12 +30,51 @@ def _salvar_catalogo(documentos: list[ContratoArmazenado]):
             dados_para_salvar.append(dicionario)
 
     json.dump(dados_para_salvar, arquivo, indent=4)
-                
-def listar_documentos() -> list[ContratoArmazenado]:
+
+#F2, F7                
+def listar_documentos(contratante: str | None = None, situacao: str | None = None,
+                      categoria: str | None = None, extensao: str | None = None) -> list[ContratoArmazenado]:
     contratos = _ler_catalogo()
+
+    if contratante:
+        lista_contratantes = []
+
+        for i in contratos:
+            if i.contratante == contratante:
+                lista_contratantes.append(i)
+
+        contratos = lista_contratantes
+
+    if situacao:
+        lista_situacoes = []
+
+        for i in contratos:
+            if i.situacao == situacao:
+                lista_situacoes.append(i)
+
+        contratos = lista_situacoes
+
+    if categoria:
+        lista_categorias = []
+
+        for i in contratos:
+            if i.categoria == categoria:
+                lista_categorias.append(i)
+
+        contratos = lista_categorias
+
+    if extensao:
+        lista_extensoes = []
+
+        for i in contratos:
+            if i.extensao == extensao:
+                lista_extensoes.append(i)
+
+        contratos = lista_extensoes
+
     return contratos
 
-def buscar_em_catalogo():
+def buscar_documento_por_id():
     ...
 
 def atualizar_catalogo():
